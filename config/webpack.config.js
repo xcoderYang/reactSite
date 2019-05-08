654880bcb744c1532bd5d29a4dacaf0526743316
TS.SZX.xcyang <xcyang@travelsky.com>
2019-05-08 17:13:43 +0800

    update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,40 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+let entry = {};
+
+let files = fs.readdirSync('src/components');
+console.log(files);
+files.forEach(file => {
+    if(file.endsWith('.js') || file.endsWith('.jsx')){
+        entry[`components/${file}`] = path.resolve(`src/components/${file}`);
+    }
+})
+
+files = fs.readdirSync('src/vendors');
+files.forEach(file => {
+    if(file.endsWith('.js') || file.endsWith('.jsx')){
+        entry[`vendors/${file}`] = path.resolve(`src/vendors/${file}`);
+    }
+})
+
+let dirs = fs.readdirSync('src/pages');
+dirs.forEach(dir => {
+    let pages = fs.readdirSync(`src/pages/${dir}`);
+    pages.forEach( page =>{
+        if(page.endsWith('.js') || page.endsWith('.jsx')){
+           entry[`pages/${dir}/${page}`] = path.resolve(`src/pages/${dir}/${page}`);
+        }
+    })
+})
+console.log(entry);
+
+
+console.log(path.resolve('./index.js'));
+
+
+
 module.exports = {
     entry: path.resolve('./index.js'),
     output: {
--- src/components/App.js
+++ src/components/App.js
@@ -131,19 +131,4 @@ class Game extends Component{
     }
 }
 
-class ShoppingList extends Component {
-    render(){
-        return(
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
-            </div>
-        )
-    }
-}
-
 export default Game;
